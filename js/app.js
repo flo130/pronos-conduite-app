2374a470313d0b857f675615b33a411f79c96f74
flo130 <flo130@hotmail.com>
2015-01-27 14:27:52 +0100

    MAJ
    MAJ

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//message d'erreur global
 var errorMsg = "Impossible de récupérer les informations...";
 
 document.addEventListener("deviceready", function() {
@@ -16,7 +17,7 @@ var app = angular.module("pcApp", [
 
 
 /**
- * Routes
+ * Routing
  */
 app.config(function($routeProvider) {
     $routeProvider
@@ -57,45 +58,81 @@ app.controller("NavigationCtrl", function($scope, $location, $routeParams, $rout
 
 //notifications controlleur
 app.controller("NotificationCtrl", function($scope, $http) {
-    var url = "/json/notif.json";
+    var url = "http://www.pronos-conduite.com/api/getNotification";
     $(".shadowing").show();
     $http.get(url)
-        .success(function(response) {
-            $scope.notifications = response;
-            $(".shadowing").hide();
-        })
-        .error(function() {
-            alert(errorMsg);
-        });
+    .success(function(response) {
+        $scope.notifications = response.notifications;
+        $(".shadowing").hide();
+    })
+    .error(function() {
+        $(".shadowing").hide();
+        alert(errorMsg);
+    });
 });
 
-//ajouter prono controlleur
-app.controller("AddPronoCtrl", function($scope, $http) {
-    //récupère les données (equipe qui jouent, cotes du match, etc...) pour charger la page
-    var url = "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/fbsS&projection=FULL";
-    //var url = "/json/prono.json";
+//classement controlleur
+app.controller("ClassementCtrl", function($scope, $http) {
+    var url = "http://www.pronos-conduite.com/api/getClassement";
     $(".shadowing").show();
     $http.get(url)
+    .success(function(response) {
+        $scope.classement = response[0];
+        $(".shadowing").hide();
+    })
+    .error(function() {
+        $(".shadowing").hide();
+        alert(errorMsg);
+    });
+});
+
+//pronostiques statistiques controlleur
+app.controller("PronostiqueCtrl", function($scope, $http) {
+    var url = "http://www.pronos-conduite.com/api/getPcClassement";
+    $(".shadowing").show();
+    $http.get(url)
+    .success(function(response) {
+        
+        $scope.pronostiques = response;
+        $(".shadowing").hide();
+    })
+    .error(function() {
+        $(".shadowing").hide();
+        alert(errorMsg);
+    });
+});
+
+//ajouter-prono controlleur
+app.controller("AddPronoCtrl", function($scope, $element, $http) {
+    //récupère les données (equipes qui jouent, cotes du match, etc...)
+    var url = "http://www.pronos-conduite.com/api/getNextMatch";
+    $(".shadowing").show();
+    $http.get(url)
+    .success(function(response) {
+        $scope.match = response;
+        $(".shadowing").hide();
+    })
+    .error(function() {
+        $(".shadowing").hide();
+        alert(errorMsg);
+    });
+
+    //prise en compte du pronostique
+    $scope.addProno = function() {
+        //poste les données vers le serveur
+        var url = "http://www.pronos-conduite.com/api/getNextMatch";
+        $(".shadowing").show();
+        $http
+        .post(url, { 
+            
+        })
         .success(function(response) {
             $scope.notifications = response;
             $(".shadowing").hide();
         })
         .error(function() {
+            $(".shadowing").hide();
             alert(errorMsg);
         });
-
-    $scope.addProno = function() {
-        //poste les données vers le serveur
-        var url = "http://maps.googleapis.com/maps/api/geocode/json?address=2%20quai%20de%20la%20pr%C3%A9valaye%2035000%20Rennes&sensor=true";
-        //var url = "/json/addProno.json";
-        $(".shadowing").show();
-        $http.get(url)
-            .success(function(response) {
-                $scope.notifications = response;
-                $(".shadowing").hide();
-            })
-            .error(function() {
-                alert(errorMsg);
-            });
     }
 });
